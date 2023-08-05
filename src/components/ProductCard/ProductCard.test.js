261a5a4401bd835bbea574bfdd5bc7311e253226
Yirang <yiranga@analuisa.com>
2023-08-05 15:36:03 -0400

    Add more tests for ProductCard
    - test Add to Cart button
    - test Buy Now button

--- src/components/ProductCard/ProductCard.test.js
+++ src/components/ProductCard/ProductCard.test.js
@@ -1,16 +1,21 @@
 import React from "react"
-import { render, screen } from "@testing-library/react"
+import { render, screen, waitFor } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
+import { useRouter } from "next/router"
 import { ProductCard } from "components"
+
+const mockPush = jest.fn()
+const mockDispatch = jest.fn()
 
 jest.mock("next/router", () => ({
   useRouter: () => ({
-    push: jest.fn(),
+    push: mockPush,
   }),
 }))
 
 jest.mock("context/MarketplaceContextProvider", () => ({
-  useMarketplaceDispatch: () => jest.fn(),
+  useMarketplaceDispatch: () => mockDispatch,
+  useMarketplaceState: () => jest.fn(),
 }))
 
 describe("ProductCard", () => {
@@ -21,13 +26,34 @@ describe("ProductCard", () => {
     vendor_id: 1,
   }
 
-  it("renders correctly", () => {
+  it("renders product name, price, and buttons correctly", () => {
     render(<ProductCard product={mockProduct} />)
     expect(screen.getByText("Test Product")).toBeInTheDocument()
-    expect(screen.getByText("100")).toBeInTheDocument()
+    expect(screen.getByText("$1.00")).toBeInTheDocument()
     expect(
       screen.getByRole("button", { name: "Add to Cart" })
     ).toBeInTheDocument()
     expect(screen.getByRole("button", { name: "Buy Now" })).toBeInTheDocument()
+  })
+
+  it("dispatches SET_CART action with correct payload when 'Add to Cart' button is clicked", async () => {
+    render(<ProductCard product={mockProduct} />)
+    userEvent.click(screen.getByRole("button", { name: "Add to Cart" }))
+
+    await waitFor(() => {
+      expect(mockDispatch).toHaveBeenCalledWith({
+        type: "SET_CART",
+        payload: mockProduct,
+      })
+    })
+  })
+
+  it("routes to /checkout when 'Buy Now' button is clicked", async () => {
+    render(<ProductCard product={mockProduct} />)
+    const { push } = useRouter()
+    userEvent.click(screen.getByRole("button", { name: "Buy Now" }))
+    await waitFor(() => {
+      expect(push).toHaveBeenCalledWith("/checkout")
+    })
   })
 })
